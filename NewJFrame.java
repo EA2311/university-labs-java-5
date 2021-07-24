895ccea54cd6ab349f38197493aaef1503a2a3ea
EA2311 <54818097+EA2311@users.noreply.github.com>
2021-07-24 22:49:17 +0300

    Update NewJFrame.java

--- NewJFrame.java
+++ NewJFrame.java
@@ -1,12 +1,4 @@
-    /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Вячеслав
- */
 import javax.swing.*;//импорт пакета swing
 //Класс исключения 
 class MyException extends Exception{
